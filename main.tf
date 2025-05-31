0e821e62101359e450d37f310f896b514fd43abb
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-05-31 15:51:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.23.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.latest
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
